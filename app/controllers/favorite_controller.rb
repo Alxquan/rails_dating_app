ae9569fc6be9be4e98fa4527e1ca2883aa2101a8
Alex Quan <alxquan@gmail.com>
2016-08-19 12:09:42 -0600

    Created controllers for Address, Favorite, Person

---
+++ app/controllers/favorite_controller.rb
@@ -0,0 +1,13 @@
+class FavoriteController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get 'favorite/index'
+
+  get 'favorite/show'
+
+  get 'favorite/edit'
+
+  get 'favorite/new'
+
+  get 'person/index'
+
+  get 'person/show'
+
+  get 'person/edit'
+
+  get 'person/new'
+
+  get 'address/index'
+
+  get 'address/show'
+
+  get 'address/new'
+
+  get 'address/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
