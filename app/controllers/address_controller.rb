9798b982811bd39be3495bd65b27c49c2dfa6e66
Carrie Estes <my4countrygirl@gmail.com>
2016-08-21 18:50:32 -0600

    small edits

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,13 +1,48 @@
 class AddressController < ApplicationController
   def index
+
   end
 
   def show
+
+  end
+
+  def create
+
   end
 
   def new
+
   end
 
   def edit
+
   end
+
+  def update
+
+  end
+
+  def destroy
+    @address.destroy
+    redirect_to ?
+  end
+
+
+
+
+  private
+    def address_params
+      params.require(??).permit(:????) # any other information will be thrown out
+    end
+
+    def find_person
+      # @person = Person.find(params[:person_id])
+    end
+
+    def find_address
+      @address = @person.address.find(params[:id])
+    end
+  end
+
 end
